e3a7574a7f6c9e1f41cfc25a2948e1a693de9401
梶祐樹 <kajiyuuki@mikado.local>
2021-04-14 20:55:54 +0900

    Chapter3

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 
-var num = 6;
-var num1 = 3;
+// じゃんけんの手をランダムに作成する関数を呼び出す
+var js_hand = getJShand();
 
-num3 = num + num1
-num4 = num - num1
-num5 = num * num1
-num6 = num / num1
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+var judge = winLose(user_hand, js_hand);
 
-alert( ' 足し算' + num3 +' 引き算' + num4 +' かけ算' + num5 + ' わり算' + num6 );
+// 結果を表示する
+if (user_hand != null){
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
+}
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  var js_hand_num = Math.floor( Math.random() * 3 );
+  var hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  var winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
